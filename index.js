56389a73710f65b716723220c15c362214d53aff
Martijn Bogaert <martijn.bogaert@hotmail.com>
2022-12-04 16:27:08 +0100

    Fetch canvasses from manifest URLs

--- index.js
+++ index.js
@@ -1,78 +1,89 @@
 const { QueryEngine } = require('@comunica/query-sparql');
+const { EventEmitter } = require('events').EventEmitter;
 
-const query = `
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX la: <https://linked.art/ns/terms/>
-PREFIX adms: <http://www.w3.org/ns/adms#>
-PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX cidoc: <http://www.cidoc-crm.org/cidoc-crm/>
+async function getManifestURLs(limit = 10, offset = 0) {
+  const comunicaEngine = new QueryEngine();
+  const querySources = ['https://stad.gent/sparql'];
 
-SELECT ?o ?title ?note ?image ?objectname ?associatie ?objectnumber ?creator ?timespan
-WHERE {
-  # object
-  ?o a cidoc:E22_Man-Made_Object.
-  
-  # title
-  #OPTIONAL { ?o cidoc:P102_has_title ?title. }
-  ?o cidoc:P102_has_title ?title.
-  
-  # note
-  OPTIONAL { ?o cidoc:P3_has_note ?note. }
-  
-  # IIIF manifest
-  OPTIONAL { ?o cidoc:P129i_is_subject_of ?image. }
-  
-  # object name
-  OPTIONAL { ?o cidoc:P41i_was_classified_by ?classified. }
-  OPTIONAL { ?classified cidoc:P42_assigned ?assigned. }
-  OPTIONAL { ?assigned skos:prefLabel ?objectname. }
-  
-  # association
-  OPTIONAL { ?o cidoc:P128_carries ?carries. }
-  OPTIONAL { ?carries cidoc:P129_is_about ?about. }
-  OPTIONAL { ?about cidoc:P2_has_type ?type. }
-  OPTIONAL { ?type skos:prefLabel ?associatie. }
-  
-  # object number
-  OPTIONAL { ?o adms:identifier ?identifier. }
-  OPTIONAL { ?identifier skos:notation ?objectnumber. }
-  
-  # creator
-  OPTIONAL { ?o cidoc:P108i_was_produced_by ?production. }
-  OPTIONAL { ?production cidoc:P14_carried_out_by ?producer. }
-  OPTIONAL { ?producer la:equivalent ?equivalent. }
-  OPTIONAL { ?equivalent rdfs:label ?creator. }
-  
-  # timespan
-  OPTIONAL { ?o cidoc:P108i_was_produced_by ?produced. }
-  OPTIONAL { ?produced cidoc:P4_has_time-span ?timespan. }
-}
-LIMIT 100
-`;
-
-async function test() {
-  const myEngine = new QueryEngine();
-  const bindingsStream = await myEngine.queryBindings(query,
-    {
-      sources: [
-        'https://stad.gent/sparql',
-        //'https://lodi.ilabt.imec.be/sparql/gent',
-      ]
+  const query = `
+    PREFIX cidoc: <http://www.cidoc-crm.org/cidoc-crm/>
+    SELECT ?manifest
+    FROM <http://stad.gent/ldes/hva>
+    WHERE { 
+      # object
+      ?object a cidoc:E22_Man-Made_Object.
+      # manifest
+      OPTIONAL { ?o cidoc:P129i_is_subject_of ?manifest. }
     }
-  );
+    LIMIT ${limit}
+    OFFSET ${offset}
+  `;
 
-  bindingsStream.on('data', (binding) => {
-    console.log(binding.toString());
-    console.log('-------------');
-  });
+  return await comunicaEngine.queryBindings(query, { sources: querySources });
+}
 
-  bindingsStream.on('end', () => {
-    console.log('Done');
-  });
+async function getManifestCanvasses(manifestURL, limit = 10, offset = 0) {
+  const comunicaEngine = new QueryEngine();
 
-  bindingsStream.on('error', (error) => {
+  const query = `
+    PREFIX iiif: <http://iiif.io/api/presentation/2#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX ns: <http://www.w3.org/2003/12/exif/ns#>
+    SELECT  *
+    WHERE {
+      ?canvas a iiif:Canvas.
+      ?canvas rdfs:label ?label.
+      ?canvas ns:height ?height.
+      ?canvas ns:width ?width.
+    }
+    LIMIT ${limit}
+    OFFSET ${offset}
+  `;
+
+  return await comunicaEngine.queryBindings(query, { sources: [manifestURL] });
+}
+
+async function workflow() {
+  const exception = 'exception';
+  const success = 'success';
+  const newManifest = 'new-manifest';
+
+  // Workflow setup
+  const workflow = new EventEmitter();
+  workflow.on(exception, (error) => {
     console.error(error);
   });
+  workflow.on(success, (response) => {
+    console.log(response);
+  });
+  workflow.on(newManifest, async (url) => {
+
+    // Get manifest canvasses
+    const canvasStream = await getManifestCanvasses(url, 1);
+    canvasStream.on('data', (canvas) => {
+      workflow.emit(success, 'NEW canvas: ' + canvas.toString());
+    })
+    canvasStream.on('end', () => {
+      workflow.emit(success, 'Canvas stream DONE');
+    })
+    canvasStream.on('error', (error) => {
+      workflow.emit(exception, 'Canvas stream ERROR' + error);
+    })
+  });
+
+  // Get manifest URLs
+  const manifestStream = await getManifestURLs();
+  manifestStream.on('data', (manifest) => {
+    const url = manifest.get('manifest').value;
+    console.log('NEW manifest:', url);
+    workflow.emit(newManifest, url);
+  });
+  manifestStream.on('end', () => {
+    workflow.emit(success, 'Manifest stream DONE');
+  });
+  manifestStream.on('error', (error) => {
+    workflow.emit(exception, 'Manifest stream ERROR' + error);
+  });
 }
 
-test();
+workflow();
