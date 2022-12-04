ab032bfe9076c00418c1e047d512b997dc08613e
Martijn Bogaert <martijn.bogaert@hotmail.com>
2022-12-04 18:47:13 +0100

    Get rid of event emmiter workflow

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,37 @@
 import { Bindings } from "@rdfjs/types";
-import { EventEmitter } from "events";
 import { CanvasQueryEngine, ManifestQueryEngine } from "./querying.js";
 
-async function workflow() {
-  const exception = "exception";
-  const success = "success";
-  const newManifest = "new-manifest";
+// Get manifest URLs
+const manifestQueryEngine = new ManifestQueryEngine();
+const canvasQueryEngine = new CanvasQueryEngine();
 
-  // Workflow setup
-  const workflow = new EventEmitter();
-  workflow.on(exception, (error: string) => {
-    console.error(error);
-  });
-  workflow.on(success, (response: string) => {
-    console.log(response);
-  });
-  workflow.on(newManifest, async (url: string) => {
-    // Get manifest canvasses
-    const canvasQueryEngine = new CanvasQueryEngine();
-    const canvasStream = await canvasQueryEngine.getManifestCanvas(url);
-    canvasStream.on("data", (canvas: Bindings) => {
-      workflow.emit(success, "NEW canvas: " + canvas.toString());
-    });
-    canvasStream.on("end", () => {
-      workflow.emit(success, "Canvas stream DONE");
-    });
-    canvasStream.on("error", (error: string) => {
-      workflow.emit(exception, "Canvas stream ERROR" + error);
-    });
+const manifestStream = await manifestQueryEngine.getManifestURLs();
+
+let counter = 1;
+
+manifestStream.on("data", async (manifest: Bindings) => {
+  const url = manifest.get("manifest").value;
+  console.log("NEW manifest:", url);
+
+  const canvasStream = await canvasQueryEngine.getManifestCanvas(url);
+
+  canvasStream.on("data", (canvas: Bindings) => {
+    console.log(counter++, ")", "NEW canvas:", canvas.toString());
   });
 
-  // Get manifest URLs
-  const manifestQueryEngine = new ManifestQueryEngine();
-  const manifestStream = await manifestQueryEngine.getManifestURLs();
-  manifestStream.on("data", (manifest: Bindings) => {
-    const url = manifest.get("manifest").value;
-    console.log("NEW manifest:", url);
-    workflow.emit(newManifest, url);
+  canvasStream.on("end", () => {
+    console.log("END canvas stream");
   });
-  manifestStream.on("end", () => {
-    workflow.emit(success, "Manifest stream DONE");
-  });
-  manifestStream.on("error", (error: string) => {
-    workflow.emit(exception, "Manifest stream ERROR" + error);
-  });
-}
 
-workflow();
+  canvasStream.on("error", (error: string) => {
+    console.error("ERR canvas stream", error);
+  });
+});
+
+manifestStream.on("end", () => {
+  console.log("END manifest stream");
+});
+
+manifestStream.on("error", (error: string) => {
+  console.error("ERR manifest stream", error);
+});
